1b6611caf4b0d3de7c6606ff6a16061631285053
Ariq Fachry R <61013150+AriqF@users.noreply.github.com>
2020-11-17 09:54:56 +0700

    Update main.java

--- Praktikum2/Pecahan/main.java
+++ Praktikum2/Pecahan/main.java
@@ -4,7 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		Pecahan a = new Pecahan(3,2);
 		Pecahan b = new Pecahan(5,6);
-		
+		System.out.println(a.getPembilang() + "/" + a.getPenyebut() + " + " + b.getPembilang() + "/" + b.getPenyebut());
 		a.tambah(b);
 
 	}
