aa4815635576d1136b5e2cb663ee55b058c32d43
Ariq Fachry R <61013150+AriqF@users.noreply.github.com>
2020-11-17 09:49:40 +0700

    Add files via upload

---
+++ Praktikum2/GameFighting/main.java
@@ -0,0 +1,30 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		GameCharacter charA = new GameCharacter("Raiden", 10, 20);
+		GameCharacter charB = new GameCharacter("SubZero", 5, 25);
+		
+		//gameStarts
+		charA.hit(charB);
+		charB.kick(charA);
+		for(int i=0; i<3; i++) {
+			charB.kick(charA);
+		}
+		for(int i=0; i<4 ;i++) {
+			charA.hit(charB);
+		}
+		
+		System.out.println(charA.getName() + " has " + charA.getLifePoint());
+		System.out.println(charB.getName() + " has " + charB.getLifePoint());
+		
+		if(charA.getLifePoint() > charB.getLifePoint()) {
+			System.out.println("Game dimenangkan oleh " + charA.getName());
+		}
+		else if(charA.getLifePoint() < charB.getLifePoint()) {
+			System.out.println("Game dimenangkan oleh " + charB.getName());
+		}
+		
+	}
+
+}
