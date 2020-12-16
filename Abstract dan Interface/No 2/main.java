67c23989096eb75b46497c68bf1002218f76e583
Ariq Fachry R <61013150+AriqF@users.noreply.github.com>
2020-12-16 20:42:34 +0700

    Update main.java

--- Abstract dan Interface/No 2/main.java
+++ Abstract dan Interface/No 2/main.java
@@ -10,16 +10,16 @@ public class main {
 		pa.setNamaPemain("Aether");
 		pa.setLevelPemain(100);
 		pa.jalankan();
-		System.out.println("Skor:" + pa.hitungSkor(3, 1));
+		System.out.println("Skor:" + pa.hitungSkor(1, 1));
 		
 		System.out.println("Permainan Strategy");
 		
 		PermainanStrategy ps = new PermainanStrategy();
 		
 		ps.setNamaPemain("Lumine");
-		ps.setLevelPemain(90);
+		ps.setLevelPemain(55);
 		ps.jalankan();
-		System.out.println("Skor:" + ps.hitungSkor(5, 0));
+		System.out.println("Skor:" + ps.hitungSkor(1, 1));
 	
 	}
 
